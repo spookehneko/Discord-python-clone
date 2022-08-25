e502cc1f5ec2d47e8755a47473ddbf8330cd51ce
Sahil Shahane <sahilpshahane123@gmail.com>
2022-08-25 10:42:18 +0530

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
-print("hiii dz")
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+from flask import Flask
+import sqlalchemy
+
+app = Flask(__name__)
+
+print(sqlalchemy.__version__)
+
+@app.get('/')
+def root():
+    return sqlalchemy.__version__
